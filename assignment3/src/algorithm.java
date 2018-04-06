d8258a71c85a528926ed8cf92483955d691a4847
Jake O'Malley <jdomalley7@gmail.com>
2018-04-06 12:14:00 -0600

    reading matrix from txt update

--- assignment3/src/algorithm.java
+++ assignment3/src/algorithm.java
@@ -14,18 +14,41 @@ import java.nio.file.Path;
 public class algorithm {
     public static void main(String [] args){
         String line;
-                Path file;
-                file = FileSystems.getDefault().getPath("src", "matrix.txt");
+        int value;
+        Path file;
+        file = FileSystems.getDefault().getPath("src", "matrix.txt");
+
+        try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8)){
+            line = null;
+            value = 0;
+            line = reader.readLine();
+            String[] labelArray = line.split(",");
+            int length = labelArray.length;
+            int matrix[][] = new int[length][length];
+            int[] temp = new int[(length*length)];
+            
+            
+            while((value = reader.read()) != -1){
+                for(int i = 0; i < temp.length; i++){
+                    temp[i] = value;
+                }
+            }
+            
+            for(int h = 0; h < temp.length; h++){
+                 System.out.println(temp[h]);
+            }
+
+            for(int i = 0; i < length; i++){
+                int iter = i;
+                for(int j = 0; j < length; j++){
+                     //matrix[i][j] = Integer.parseInt(temp[iter]);
+                     iter += length;
+                     System.out.println(matrix[i][j]);
+                }
+
+            }
                 
-                try (BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8)){
-                    line = null;
-                    line = reader.readLine();
-                    String[] labelArray = line.split(",");
-                    for(int i = 0; i < labelArray.length; i++){
-                        System.out.println(labelArray[i]);
-                    }
-                    
-                }catch (IOException x){
+        }catch (IOException x){
             System.err.format("IOException: %s%n", x);
         }
     }
