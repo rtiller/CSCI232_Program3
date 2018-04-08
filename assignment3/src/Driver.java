01f093b064b528ff9a90b9a419a4c2db57c9f6dc
rtiller <33472584+rtiller@users.noreply.github.com>
2018-04-08 17:09:17 -0600

    Driver and Algorithms Classes Update
    I have created a driver to manage what we want to do. Sweet and simple.
    I have the algorithms reading in from the .txt file right now, and creating a 2D int array for our matrix as well as a String array for our labels.
    
    I have also created several methods to print the arrays for trouble shooting as well as one that creates a priority queue and identifies the min edge value from the 2D array.

---
+++ assignment3/src/Driver.java
@@ -0,0 +1,21 @@
+/*
+ * Authors: Jake O'Malley, Robert Tiller
+ * Date: 4/10/2018
+ * Overview:
+*/
+
+public class Driver {
+    
+    public static void main(String [] args)
+    {
+       algorithm test = new algorithm();
+       test.printLabelArray();
+       test.printIntArray();
+       test.printMinEdge(1);
+       
+       
+        
+    }
+
+    
+}
